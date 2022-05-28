b2285fbc8f5947422ddf6ff8aded3129d810fdce
GraceCee <98286092+GraceCee@users.noreply.github.com>
2022-05-28 15:34:56 +0800

    adding flyweightTest file

--- src/test/kotlin/structural/flyweight/MainKtTest.kt
+++ src/test/kotlin/structural/flyweight/MainKtTest.kt
@@ -1,5 +1,7 @@
 package structural.flyweight
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -7,6 +9,10 @@ import org.junit.jupiter.api.Assertions.*
 internal class MainKtTest {
 
     @Test
-    fun main() {
+    fun setColor() {
+        val yellowThinPen1 = PenFactory.getThickPen("YELLOW") //created new pen
+        assertThat(yellowThinPen1!!.setColor("YELLOW")).isEqualTo(yellowThinPen1.draw("Hello World !!"))
+
     }
+
 }
